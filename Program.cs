1eab0ed9be72d83aa6962c9dd10bad9bacc7d3a9
Kamil <mentos.96@o2.pl>
2023-06-21 21:39:58 +0200

    Wriat two static functions to add random card to list and show on console  contents list

--- Program.cs
+++ Program.cs
@@ -1,7 +1,36 @@
-﻿internal class Program
+﻿using Game_Card;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        List<Card> ListCard = new List<Card>();
+
+        for(int i= 0; i < random.Next(1, 15); i++)
+        {
+            ListCard.Add(RandomCard(random));
+        }
+        
+        PrintCards(ListCard);
+
+        
+    }
+    static Random random = new Random();
+
+    static Card RandomCard( Random rand)
+    {
+        Card card = new Card((EValue)rand.Next(1, 14), (ESuit)rand.Next(4));
+        return card;
+
+    }
+
+    static void PrintCards(List<Card> cards)
+    {
+        int i = cards.Count;
+        for(int j =0; j < i; j++)
+        {
+            Console.WriteLine($"{j}: {cards[j].Name}");
+        }
     }
 }
